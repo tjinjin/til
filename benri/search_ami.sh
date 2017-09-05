b65d1ff259690ad8c285a451f119493f0fbf45ee
tjinjin <tjinjinprogram@gmail.com>
2017-09-05 10:46:20 +0900

    Remove

--- benri/search_ami.sh
+++
@@ -1,8 +0,0 @@
-# 最新のAMIを取得する
-aws ec2 describe-images \
-  --filters "Name=platform,Values=windows" \
-  --owners amazon  | \
-  jq ".Images[] | select(.Name | contains(\"Windows_Server-2012\") \
-        and contains(\"-64Bit-Base-\") \
-        and contains(\"Japanese\")) | \
-        {\"ImageId\":.ImageId, \"Name\":.Name, \"Description\":.Description}"
