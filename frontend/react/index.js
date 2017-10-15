dc8ac8eb4fbfac2829f9b75e42eb13d1e2f971ed
tjinjin <tjinjinprogram@gmail.com>
2017-10-15 23:46:50 +0900

    Prepare to implement react

--- frontend/react/index.js
+++ frontend/react/index.js
@@ -1,6 +1,38 @@
-import { createStore } from 'redux'
+import { createStore } from 'redux';
+import { addTodo, toggeTodo, setVisibilityFilter } from './actions/index.js';
 
 let store = createStore(function() { return 'Hello Redux' })
 
-var contents = document.getElementById('contents')
-contents.innerHTML = store.getState().toString()
+let store = creataStore(() => { return 'Hello' });
+
+// TODOの追加
+var addTodoElem = document.getElementById('addTodo');
+var input = addTodoElem.getElementByTagName('input')[0];
+var button = addTodoElem.getElementByTagName('button')[0];
+button.addEventListner('click', () => {
+  // ボタンをクリックしたら「TODOを追加する」というアクションをStoreに渡す
+  var todoText = input.value;
+  store.dispatch(addTodo(todoText));
+});
+
+// TODOの完了
+var todoList = document.getElementById('todoList');
+var elements = todoList.getElementsByTagName('li');
+var listArray = [...elements];
+listArray.forEach(v, index) => {
+  v.addEventListner('click', e => {
+    // TODOをクリックしたら「TODOの完了状態を切り替える」というアクションをStoreに渡す
+    store.dispatch(toggeTodo(index));
+  })
+}
+
+// フィルタリング
+var links = ducument.getElementById('links');
+var childs = list.childNodes;
+var childList = [...childs];
+childList.filter(v => v.nodeName != '#text').forEach(v => {
+  v.addEventListner('click', e => {
+    var filterText = v.innerHTML;
+    store.dispatch(setVisibilityFilter(filterText));
+  });
+});
